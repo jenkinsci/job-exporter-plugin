85cbe87e449a55321cc2b290c867dbe11da66f13
m31 <m31@dev.java.net>
2011-01-31 19:58:05 +0000

    error correction due to an error report by Hendryk

--- src/main/java/com/meyling/hudson/plugin/job_exporter/ExporterBuilder.java
+++ src/main/java/com/meyling/hudson/plugin/job_exporter/ExporterBuilder.java
@@ -68,88 +68,98 @@ public class ExporterBuilder extends Builder {
     @Override
     public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) {
         // this is where you 'build' the project
-    	String prefix = "build.";
-    	Properties export = new Properties();
+        String prefix = "build.";
+        Properties export = new Properties();
         try {
-        	System.out.println(Computer.currentComputer().getHostName());
-        	export.put(prefix + "hudson.version", StringUtils.defaultString(build.getHudsonVersion()));
-        	export.put(prefix + "id", StringUtils.defaultString(build.getId()));
-        	Executor executor = build.getExecutor();
-        	if (executor != null) {
-	        	export.put(prefix + "summary", StringUtils.defaultString(build.getExecutor().getName()));
-        	}
-        	export.put(prefix + "number", "" + build.getNumber());
-        	
-        	EnvVars env = build.getEnvironment(new LogTaskListener(Logger.getLogger(this.getClass().getName()), Level.INFO));
-        	
-        	if (env != null) {
-	        	export.put(prefix + "jobName", StringUtils.defaultString(env.get("JOB_NAME")));
-	        	export.put(prefix + "cvsBranch", StringUtils.defaultString(env.get("CVS_BRANCH")));
-        	}
+            System.out.println(Computer.currentComputer().getHostName());
+            export.put(prefix + "hudson.version", StringUtils.defaultString(build.getHudsonVersion()));
+            export.put(prefix + "id", StringUtils.defaultString(build.getId()));
+            Executor executor = build.getExecutor();
+            if (executor != null) {
+                export.put(prefix + "summary", StringUtils.defaultString(build.getExecutor().getName()));
+            }
+            export.put(prefix + "number", "" + build.getNumber());
+            
+            EnvVars env = build.getEnvironment(new LogTaskListener(Logger.getLogger(this.getClass().getName()), Level.INFO));
+            
+            if (env != null) {
+                export.put(prefix + "jobName", StringUtils.defaultString(env.get("JOB_NAME")));
+                export.put(prefix + "cvsBranch", StringUtils.defaultString(env.get("CVS_BRANCH")));
+            }
             log(listener.getLogger(), "exporting properties");
             
             Mailer.DescriptorImpl descriptor = (Mailer.DescriptorImpl) Hudson.getInstance().getDescriptorByType(
-            	Mailer.DescriptorImpl.class);
+                Mailer.DescriptorImpl.class);
             export.put(prefix + "admin.emailAddress", descriptor.getAdminAddress());
-            // set admin email address as fallback, if we don't get a user email address 
-            export.put(prefix + "user.emailAddress", descriptor.getAdminAddress());
 
             // now we iterate over the cause actions to identify the user and get his properties
             List<CauseAction> cal = build.getActions(CauseAction.class);
             for (CauseAction ca : cal) {
-            	log(listener.getLogger(), ca.getDisplayName());
-            	log(listener.getLogger(), ca.toString());
-            	List<Cause> cl = ca.getCauses();
+                log(listener.getLogger(), ca.getDisplayName());
+                log(listener.getLogger(), ca.toString());
+                List<Cause> cl = ca.getCauses();
                 for (Cause c : cl) {
-                	if (c instanceof UserCause) {
-                		final UserCause uc = (UserCause) c;
-                		final String authenticationName = Hudson.getAuthentication().getName();
-                    	export.put(prefix + "user.name", StringUtils.defaultString(authenticationName));
-                    	User u = User.get(authenticationName);
-                    	export.put(prefix + "user.fullName", StringUtils.defaultString(u.getFullName()));
-                    	Mailer.UserProperty umail = u.getProperty(Mailer.UserProperty.class);
-                    	String email = StringUtils.defaultString(umail.getAddress()).trim();
-                    	if (email.length() > 0) {
-                    		export.put(prefix + "user.emailAddress", email);
-                    	}
-                	} else if (c instanceof UpstreamCause) {
-                		final UpstreamCause uc = (UpstreamCause) c; 
-                    	export.put(prefix + "upstream.number", "" + uc.getUpstreamBuild());
-                    	export.put(prefix + "upstream.project", StringUtils.defaultString(uc.getUpstreamProject()));
-                	} else if (c instanceof RemoteCause) {
-                		final RemoteCause uc = (RemoteCause) c;
-                		String host = uc.getShortDescription();	// fallback
-                		try {
-                			host = (String) getFieldValue(uc, "host");
-                		} catch (Exception e) {
-                			// ignore
-                		}
-                    	export.put(prefix + "remote.host", "" + host);
-                		String note = uc.getShortDescription();	// fallback
-                		try {
-                			note = (String) getFieldValue(uc, "note");
-                		} catch (Exception e) {
-                			// ignore
-                		}
-                    	export.put(prefix + "remote.note", "" + note);
-                	}
+                    if (c instanceof UserCause) {
+                        // set admin email address as fallback, if we don't get a user email address 
+                        export.put(prefix + "user.emailAddress", descriptor.getAdminAddress());
+                        final UserCause uc = (UserCause) c;
+                        export.put(prefix + "user.name", uc.getUserName());
+                        String authenticationName = Hudson.getAuthentication().getName();
+                        try {
+                            authenticationName = (String) getFieldValue(uc, "authenticationName");
+                        } catch (Exception e) {
+                            // ignore
+                        }
+                        export.put(prefix + "user.name", StringUtils.defaultString(authenticationName));
+                        try {
+                            User u = User.get(authenticationName);
+                            export.put(prefix + "user.fullName", StringUtils.defaultString(u.getFullName()));
+                            Mailer.UserProperty umail = u.getProperty(Mailer.UserProperty.class);
+                            String email = StringUtils.defaultString(umail.getAddress()).trim();
+                            if (email.length() > 0) {
+                                export.put(prefix + "user.emailAddress", email);
+                            }
+                        } catch (Exception e) {
+                            // ignore
+                        }
+                    } else if (c instanceof UpstreamCause) {
+                        final UpstreamCause uc = (UpstreamCause) c; 
+                        export.put(prefix + "upstream.number", "" + uc.getUpstreamBuild());
+                        export.put(prefix + "upstream.project", StringUtils.defaultString(uc.getUpstreamProject()));
+                    } else if (c instanceof RemoteCause) {
+                        final RemoteCause uc = (RemoteCause) c;
+                        String host = uc.getShortDescription(); // fallback
+                        try {
+                            host = (String) getFieldValue(uc, "host");
+                        } catch (Exception e) {
+                            // ignore
+                        }
+                        export.put(prefix + "remote.host", "" + host);
+                        String note = uc.getShortDescription(); // fallback
+                        try {
+                            note = (String) getFieldValue(uc, "note");
+                        } catch (Exception e) {
+                            // ignore
+                        }
+                        export.put(prefix + "remote.note", "" + note);
+                    }
                 }
             }
             FilePath ws = build.getWorkspace();
             FilePath hudson = ws.child("hudsonBuild.properties");
             if (hudson.exists()) {
-            	if (!hudson.delete()) {
-            		log(listener.getLogger(), "old file can not be deleted: " + hudson);
-                	build.setResult(Result.FAILURE);
-                	return false;
-            	} else {
-                	log(listener.getLogger(), "old file deleted: " + hudson);
-            	}
+                if (!hudson.delete()) {
+                    log(listener.getLogger(), "old file can not be deleted: " + hudson);
+                    build.setResult(Result.FAILURE);
+                    return false;
+                } else {
+                    log(listener.getLogger(), "old file deleted: " + hudson);
+                }
             }
             final OutputStream out = hudson.write();
             export.store(out, "created by " + this.getClass().getName());
             out.close();
-    		log(listener.getLogger(), "new properties file written: " + hudson);
+            log(listener.getLogger(), "new properties file written: " + hudson);
         } catch (IOException e) {
             e.printStackTrace(listener.error("failed to read or write property file"));
             build.setResult(Result.FAILURE);
@@ -158,13 +168,13 @@ public class ExporterBuilder extends Builder {
             e.printStackTrace(listener.error("failed to read or write property file"));
             build.setResult(Result.FAILURE);
             return false;
-		}
+        }
         return true;
     }
 
-	protected void log(final PrintStream logger, final String message) {
-		logger.println("[exporter] " + StringUtils.defaultString(message));
-	}
+    protected void log(final PrintStream logger, final String message) {
+        logger.println("[exporter] " + StringUtils.defaultString(message));
+    }
 
     
     // overrided for better type safety.
@@ -222,7 +232,7 @@ public class ExporterBuilder extends Builder {
         public boolean configure(StaplerRequest req, JSONObject o) throws FormException {
             // to persist global configuration information,
             // set that to properties and call save().
-        	// still nothing to do for us
+            // still nothing to do for us
             save();
             return super.configure(req, o);
         }
@@ -261,7 +271,7 @@ public class ExporterBuilder extends Builder {
      * @throws  NoSuchFieldException    Field with name <code>name</code> was not found.
      */
     @SuppressWarnings("unchecked")
-	public static Field getField(final Object obj, final String name) throws NoSuchFieldException {
+    public static Field getField(final Object obj, final String name) throws NoSuchFieldException {
         Field field = null;
         try {
             Class cl = obj.getClass();
@@ -284,4 +294,3 @@ public class ExporterBuilder extends Builder {
     }
 
 }
-
